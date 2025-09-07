674dd5c5b93b59f0da495b330d295c791aca2801
Joshua Moore <joshua.moore@verticesandedges.net>
2025-09-07 04:39:13 -0700

    missing image

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,4 +2,12 @@ module.exports = {
   content: [
     "./src/**/*.{html,ts}"
   ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'spiral-background': "url('/assets/images/spiral-small.png')"
+      }
+    }
+  },
+  plugins: [],
 }
